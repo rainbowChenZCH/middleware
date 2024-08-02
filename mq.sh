8633241d97cf9984a9af29bd9bf3eed065d1192c
rainyrainbow <1092168575@qq.com>
2024-08-02 19:03:45 +0800

    new command

--- mq.sh
+++ mq.sh
@@ -15,4 +15,40 @@ python3 mq_pika-server.py
 python3 mq_pika-client.py
 
 
+# rabbitmqctl命令
+# 查看用户
+rabbitmqctl list_users
+# 添加用户
+rabbitmqctl add_user admin admin
+# 设置用户标签
+rabbitmqctl set_user_tags admin administrator
+# 设置用户权限
+rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+# 删除用户
+rabbitmqctl delete_user admin
+# 查看vhost
+rabbitmqctl list_vhosts
+# 添加vhost
+rabbitmqctl add_vhost my_vhost
+# 删除vhost
+rabbitmqctl delete_vhost my_vhost
+# 查看权限
+rabbitmqctl list_permissions
+# 设置权限
+rabbitmqctl set_permissions -p my_vhost admin ".*" ".*" ".*"
+# 查看队列
+rabbitmqctl list_queues
+# 查看交换机
+rabbitmqctl list_exchanges
+# 查看绑定
+rabbitmqctl list_bindings
+# 查看连接
+rabbitmqctl list_connections
+# 查看通道
+rabbitmqctl list_channels
+# 查看节点
+rabbitmqctl cluster_status
+
+
+
  
